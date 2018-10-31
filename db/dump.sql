042bb6072fd45cf74907793847250ac0942875c4
Anatoly <east@thyloved.ru>
2018-10-31 17:21:36 +0300

    Database changes

--- db/dump.sql
+++ db/dump.sql
@@ -5,7 +5,30 @@
 -- Dumped from database version 9.5.14
 -- Dumped by pg_dump version 10.5 (Ubuntu 10.5-1.pgdg16.04+1)
 
--- Started on 2018-10-31 17:14:21 MSK
+-- Started on 2018-10-31 17:20:59 MSK
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+SET row_security = off;
+
+DROP DATABASE infomat_qt;
+--
+-- TOC entry 2528 (class 1262 OID 16407660)
+-- Name: infomat_qt; Type: DATABASE; Schema: -; Owner: postgres
+--
+
+CREATE DATABASE infomat_qt WITH TEMPLATE = template0 ENCODING = 'UTF8' LC_COLLATE = 'ru_RU.UTF-8' LC_CTYPE = 'ru_RU.UTF-8';
+
+
+ALTER DATABASE infomat_qt OWNER TO postgres;
+
+\connect infomat_qt
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -3107,7 +3130,7 @@ GRANT ALL ON SCHEMA public TO postgres;
 GRANT ALL ON SCHEMA public TO PUBLIC;
 
 
--- Completed on 2018-10-31 17:14:21 MSK
+-- Completed on 2018-10-31 17:20:59 MSK
 
 --
 -- PostgreSQL database dump complete
