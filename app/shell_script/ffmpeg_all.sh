7110fb877a49113a504765046cf1a0bc6576e0b3
Anatoly <east@thyloved.ru>
2018-11-27 18:24:52 +0300

    Video converting shell script fix

--- app/shell_script/ffmpeg_all.sh
+++ app/shell_script/ffmpeg_all.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
-directory='/var/www/infomat/app/webroot/video/'
-files=$(find $(directory) -type f -name "*.mp4")
-for f in $files; 
+
+directory='/var/www/infomat/app/webroot/video/';
+cd $directory;
+files=$(find $(directory) -type f -name "*.mp4");
+
+echo "Files list: "$files; 
+# Run converting
+for f in $files;
 	do
-		ffmpeg -i $f ${f%.mp4}.mp4.webm;
-		rm -f $f;
-done 
+	echo "Converting file: "$f; 
+	ffmpeg -i $f ${f%.mp4}.mp4.webm;
+	echo "Removing file: "$f;
+	rm -f $f;
+done
+
+t=$(date '+%d/%m/%Y %H:%M:%S');
+echo "DONE: "$t
